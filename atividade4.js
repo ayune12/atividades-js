a9f8adf41d3460e32884e518354a3bcdf1634329
happy <166138596+ayune12@users.noreply.github.com>
2026-03-03 12:11:29 -0300

    Create atividade4.js

---
+++ atividade4.js
@@ -0,0 +1,11 @@
+while(true){
+
+let a = Number(prompt("Digite um número:"));
+
+let contador = 1;
+
+while(contador <= 10){
+    console.log(a + " X " + contador + " = " + (a * contador));
+    contador++;
+}
+}
